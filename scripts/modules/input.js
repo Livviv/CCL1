421b8fc960ddda402f940db0c55882224acc1b25
Livviv <cc241076@fhstp.ac.at>
2025-02-02 09:10:02 +0100

    make arrow keys usable

--- scripts/modules/input.js
+++ scripts/modules/input.js
@@ -3,13 +3,13 @@ import { global } from "./global.js";
 function move(event) {
 
     //Example Movement for the PacMan Game
-    if (event.code === 'Space' || event.key === 'w') {
+    if (event.code === 'Space' || event.key === 'w' || event.key === 'ArrowUp') {
         global.playerObject.startJump();
     }
 }
 
 function stop(event) {
-    if (event.code === 'Space' || event.key === 'w') {
+    if (event.code === 'Space' || event.key === 'w' || event.key === 'ArrowUp') {
         global.playerObject.doJump();
     }
 }
