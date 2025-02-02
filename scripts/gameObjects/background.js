9fe8ae64b85ca6120ec4989c0b51068b6fbead44
Livviv <cc241076@fhstp.ac.at>
2025-02-02 09:08:43 +0100

    create background.js

---
+++ scripts/gameObjects/background.js
@@ -0,0 +1,26 @@
+import { BaseGameObject } from "./baseGameObject.js";
+import { global } from "../modules/global.js";
+
+class Background extends BaseGameObject {
+    name = "Background";
+    htmlElement = null;
+    xVelocity = 200;
+    yVelocity = 0;
+    
+    update = function() {
+       /*  console.log(this.x, this.y); */
+        this.x += this.xVelocity * global.deltaTime;
+        this.htmlElement.style.backgroundPositionX = -this.x + "px";
+       
+    }
+
+    draw = function () {}
+
+    constructor(x, y, width, height) {
+        super(x, y, width, height);
+        this.htmlElement = document.querySelector("#background");
+      
+    }
+}
+
+export {Background}
